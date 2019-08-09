0aaebaacc610a308d7024daff9864ad8e3ce4315
Sanuja91 <sanujac@live.com>
2019-08-09 22:28:38 +0530

    Operation Arguments

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import { GraphQLServer } from "graphql-yoga"
 // Type definitions (schema)
 const typeDefs = `
   type Query {
-    me: Post!
+    add(a: Float!, b: Float!): Float!
+    greeting(name: String, position: String): String!
+    me: User!
+    post: Post!
+  }
+
+  type User {
+    id:ID!
+    name: String!
+    email: String!
+    age: Int
+
   }
 
   type Post {
@@ -16,11 +27,25 @@ const typeDefs = `
 `
 const resolvers = {
   Query: {
+    add(parent, args, ctx, info){
+      return args.a + args.b
+    },
+    greeting(parent, args, ctx, info) {
+      if (args.name) return `Hello ${args.name}! You are my ${args.position}`
+      else return "Hello!"
+    },
     me() {
-      return{
-        id:'123123',
-        title:'Lord of the Rings',
-        body:'Return of the King',
+      return {
+        id: "12321312",
+        name: "Sanuja",
+        email: "sadasd@live.com"
+      }
+    },
+    post() {
+      return {
+        id: "123123",
+        title: "Lord of the Rings",
+        body: "Return of the King",
         published: false
       }
     }
